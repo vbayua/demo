c4871321616930d545cd93a411c6aba71617d71e
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-28 09:01:04 +0700

    Tweak vue and tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
-import './bootstrap';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+    progress: {
+        // The delay after which the progress bar will appear, in milliseconds...
+        delay: 250,
+
+        // The color of the progress bar...
+        color: '#29d',
+
+        // Whether to include the default NProgress styles...
+        includeCSS: true,
+
+        // Whether the NProgress spinner will be shown...
+        showSpinner: false,
+    },
+});
